113bde9aeadca72cc07ff7b493d2863847e0b1a1
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-03-06 13:50:49 +0000

    base

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  media: {
+    type: {
+      type: String,
+      enum: ["image", "video"],
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    cloudinaryId: {
+      type: String,
+      required: true,
+    },
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
