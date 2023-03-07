e781a4776fe07bbb77e701296e0663993d829526
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-03-07 12:22:20 +0000

    video update

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,16 @@
 const multer = require("multer");
 const path = require("path");
 
+const multer = require("multer");
+
 module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".mp4" && ext != ".m4v," && ext !== ".avi" && ext !== ".mov") {
+    if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("video/")) {
+      cb(null, true);
+    } else {
       cb(new Error("File type is not supported"), false);
-      return;
     }
-    cb(null, true);
   },
 });
+
