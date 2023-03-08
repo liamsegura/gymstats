d9a53b4a8639b1d5f0d46bf77b0add0ba992ceee
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-03-08 16:11:14 +0000

    video upload error json

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Body Parsing
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
-app.use(express.json({
-  limit: '50mb'
-}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 //Logging
 app.use(logger("dev"));
 
