08f61dd0acfbdc56b2c635eec5d5c558c2f50aff
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-03-14 14:53:49 +0000

    would help if id actually save my editProfile ejs file

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ const { ensureAuth, ensureGuest, ensureUser } = require("../middleware/auth");
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile/:id", ensureAuth, postsController.getProfile);
-router.get("/editProfile/:id", ensureAuth, postsController.editProfile)
+router.get("/editProfile/:id", ensureAuth, ensureUser, postsController.editProfile)
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get('/leaderboard', ensureAuth, leaderboardController.getLeaderboard);
 router.get("/postMenu", ensureAuth, postsController.getPostMenu)
