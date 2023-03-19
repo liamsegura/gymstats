862a1456f70a7c200d1d47eec5c91668f285d787
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-03-19 17:39:17 +0000

    catagory select by bodyweight added

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,20 +1,23 @@
 const PR = require('../models/PR');
 const User = require("../models/User");
 
-  module.exports = {
-getLeaderboard: async (req, res) => {
-
-  try {
-    let selectedCategory = req.query.category || '';
-    const prsByCategory = await PR.find()
-    let prs = await PR.find(selectedCategory ? { category: selectedCategory } : {})
-      .sort({ weight: -1 })
-      .limit(100)
-      .populate({ path: 'user', model: 'User' });
-    res.render('leaderboard', { prsByCategory, prs, selectedCategory, loggedUser: req.user, onNotificationsPage: false});
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
+module.exports = {
+  getLeaderboard: async (req, res) => {
+    try {
+      let selectedCategory = req.query.category || '';
+      let selectedBodyweight = req.query.bodyweight || '';
+      const prsByCategory = await PR.find({ category: selectedCategory });
+      let prs = await PR.find(selectedCategory ? { category: selectedCategory } : {})
+        .sort({ weight: -1 })
+        .limit(100)
+        .populate({ path: 'user', model: 'User' });
+      if (selectedBodyweight) {
+        prs = prs.filter(pr => pr.bodyweight === Number(selectedBodyweight));
+      }
+      res.render('leaderboard', { prsByCategory, prs, selectedCategory, selectedBodyweight, loggedUser: req.user, onNotificationsPage: false });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
   }
-}
 };
