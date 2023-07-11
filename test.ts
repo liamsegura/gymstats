cdac524b19d7428f734aa898ca58cf3bf162e8aa
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-11 17:04:08 +0100

    update readme, typescript conversion

---
+++ test.ts
@@ -0,0 +1,356 @@
+namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  namespace NodeJS {
+    interface ProcessEnv {
+      CLOUD_NAM: string;
+      PORT: string;
+      DB_STRING: string;
+      API_KEY:string;
+      API_SECRET:string;          
+    }
+  }
+  
