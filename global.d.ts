1bdc6f055342c2c610ab73fab138e55f76c552c1
Liam Segura <102547056+liamsegura@users.noreply.github.com>
2023-07-11 16:50:35 +0100

    typescript-conversion

--- global.d.ts
+++ global.d.ts
@@ -7,3 +7,167 @@ namespace NodeJS {
       API_SECRET:string;          
     }
   }
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+/*~ If this library is callable (e.g. can be invoked as myLib(3)),
+ *~ include those call signatures here.
+ *~ Otherwise, delete this section.
+ */
+ declare function myLib(a: string): string;
+ declare function myLib(a: number): number;
+ /*~ If you want the name of this library to be a valid type name,
+  *~ you can do so here.
+  *~
+  *~ For example, this allows us to write 'var x: myLib';
+  *~ Be sure this actually makes sense! If it doesn't, just
+  *~ delete this declaration and add types inside the namespace below.
+  */
+ interface myLib {
+   name: string;
+   length: number;
+   extras?: string[];
+ }
+ /*~ If your library has properties exposed on a global variable,
+  *~ place them here.
+  *~ You should also place types (interfaces and type alias) here.
+  */
+ declare namespace myLib {
+   //~ We can write 'myLib.timeout = 50;'
+   let timeout: number;
+   //~ We can access 'myLib.version', but not change it
+   const version: string;
+   //~ There's some class we can create via 'let c = new myLib.Cat(42)'
+   //~ Or reference e.g. 'function f(c: myLib.Cat) { ... }
+   class Cat {
+     constructor(n: number);
+     //~ We can read 'c.age' from a 'Cat' instance
+     readonly age: number;
+     //~ We can invoke 'c.purr()' from a 'Cat' instance
+     purr(): void;
+   }
+   //~ We can declare a variable as
+   //~   'var s: myLib.CatSettings = { weight: 5, name: "Maru" };'
+   interface CatSettings {
+     weight: number;
+     name: string;
+     tailLength?: number;
+   }
+   //~ We can write 'const v: myLib.VetID = 42;'
+   //~  or 'const v: myLib.VetID = "bob";'
+   type VetID = string | number;
+   //~ We can invoke 'myLib.checkCat(c)' or 'myLib.checkCat(c, v);'
+   function checkCat(c: Cat, s?: VetID);
+ }
+ const maxInterval = 12;
+function getArrayLength(arr) {
+  return arr.length;
+}
+module.exports = {
+  getArrayLength,
+  maxInterval,
+};
+
+export function getArrayLength(arr: any[]): number;
+export const maxInterval: 12;
+export function getArrayLength(arr) {
+  return arr.length;
+}
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+/*~ This is the module template file. You should rename it to index.d.ts
+ *~ and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+/*~ If this module is a UMD module that exposes a global variable 'myLib' when
+ *~ loaded outside a module loader environment, declare that global here.
+ *~ Otherwise, delete this declaration.
+ */
+ export as namespace myLib;
+ /*~ If this module exports functions, declare them like so.
+  */
+ export function myFunction(a: string): string;
+ export function myOtherFunction(a: number): number;
+ /*~ You can declare types that are available via importing the module */
+ export interface SomeType {
+   name: string;
+   length: number;
+   extras?: string[];
+ }
+ /*~ You can declare properties of the module using const, let, or var */
+ export const myField: number;
+ // Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+/*~ This is the global-modifying module template file. You should rename it to index.d.ts
+ *~ and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+/*~ Note: If your global-modifying module is callable or constructable, you'll
+ *~ need to combine the patterns here with those in the module-class or module-function
+ *~ template files
+ */
+declare global {
+  /*~ Here, declare things that go in the global namespace, or augment
+   *~ existing declarations in the global namespace
+   */
+  interface String {
+    fancyFormat(opts: StringFormatOptions): string;
+  }
+}
+/*~ If your module exports types or values, write them as usual */
+export interface StringFormatOptions {
+  fancinessLevel: number;
+}
+/*~ For example, declaring a method on the module (in addition to its global side effects) */
+export function doSomething(): void;
+/*~ If your module exports nothing, you'll need this line. Otherwise, delete it */
+export {};
+interface Foo {
+  x: number;
+}
+// ... elsewhere ...
+interface Foo {
+  y: number;
+}
+
+console.log(a.x + a.y); // OK
+interface Foo {
+  x: number;
+}
+// ... elsewhere ...
+interface Foo {
+  y: number;
+}
+
+console.log(a.x + a.y); // OK
+class Foo {
+  x: number;
+}
+// ... elsewhere ...
+interface Foo {
+  y: number;
+}
+
+console.log(a.x + a.y); // OK
+class C {}
+// ... elsewhere ...
+namespace C {
+  export interface D {}
+}
+let y: C.D; // OK
+namespace X {
+  export interface Y {}
+  export class Z {}
+}
+// ... elsewhere ...
+namespace X {
+  export var Y: number;
+  export namespace Z {
+    export class C {}
+  }
+}
+type X = string;
